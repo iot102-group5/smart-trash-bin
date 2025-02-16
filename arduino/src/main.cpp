aa58af6f3496a667e933cc332ce37265dff023f7
JustPhong <nguyengiaphong301005@gmail.com>
2025-02-16 19:52:37 +0700

    feat: detect object and open trash can accordingly

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,24 +1,53 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <LoRa.h>
+#include <Servo.h>
 
-#define TRIG_PIN 7
-#define ECHO_PIN 6
+#define OBJECT_TRIG_PIN 8
+#define OBJECT_ECHO_PIN 7
+#define TRASH_TRIG_PIN 6
+#define TRASH_ECHO_PIN 5
+#define SERVO_PIN 3
+
 #define TRASH_CAN_HEIGHT 40
+#define OPEN_DISTANCE 10
 
-#define LORA_SS 10
-#define LORA_RST 9
-#define LORA_DIO0 2
+Servo servo;
+float trash_level;
+bool is_open;
 
-float get_distance()
+void onTxDone() {
+  Serial.println("Send data success");
+}
+
+void setup()
 {
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
+  Serial.begin(9600);
 
-  float duration = pulseIn(ECHO_PIN, HIGH);
+  pinMode(OBJECT_TRIG_PIN, OUTPUT);
+  pinMode(OBJECT_ECHO_PIN, INPUT);
+
+  pinMode(TRASH_TRIG_PIN, OUTPUT);
+  pinMode(TRASH_ECHO_PIN, INPUT);
+
+  servo.attach(SERVO_PIN, 600, 2300);
+
+  if (!LoRa.begin(915E6))
+  {
+    Serial.println("LoRa init failed!");
+  }
+  LoRa.onTxDone(onTxDone);
+}
+
+float get_distance(int trig_pin, int echo_pin)
+{
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+
+  float duration = pulseIn(echo_pin, HIGH);
   float distance = duration / 29 / 2;
 
   return distance;
@@ -26,35 +55,36 @@ float get_distance()
 
 float get_trash_level()
 {
-  float distance = get_distance();
-  float trash_level = ((TRASH_CAN_HEIGHT - distance) / TRASH_CAN_HEIGHT) * 100;
-
+  float distance = get_distance(TRASH_TRIG_PIN, TRASH_ECHO_PIN);
+  float trash_level = (1 - distance / TRASH_CAN_HEIGHT) * 100;
   trash_level = constrain(trash_level, 0, 100);
   return trash_level;
 }
 
-void lora_transmit(float trash_level){
-  LoRa.beginPacket();
-  LoRa.print(trash_level);
-  LoRa.endPacket();
+bool get_is_open() {
+  return get_distance(OBJECT_TRIG_PIN, OBJECT_ECHO_PIN) <= OPEN_DISTANCE;
 }
 
-void setup()
-{
-  Serial.begin(9600);
-    
-    pinMode(TRIG_PIN, OUTPUT);
-    pinMode(ECHO_PIN, INPUT);
+void set_trash_can(bool is_open) {
+  servo.write(is_open? 90 : 0);
+}
 
-    LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
-    
-    if (!LoRa.begin(915E6)) {
-        Serial.println("LoRa init failed!");
-    }
-    Serial.println("LoRa Initialized");
+
+void lora_transmit(float trash_level, bool is_open)
+{
+  LoRa.beginPacket();
+  LoRa.println(trash_level);
+  LoRa.println(is_open);
+  LoRa.endPacket(true);
 }
 
 void loop()
 {
-
+  trash_level = get_trash_level();
+  Serial.println(trash_level);
+  is_open = get_is_open();
+  Serial.println(is_open);
+  set_trash_can(is_open);
+  lora_transmit(trash_level, is_open);
+  delay(1000);
 }
