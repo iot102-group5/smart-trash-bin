9cd46f8a2c78f1e54ddc7306fcccfb35c543bef6
akagiyuu <khanghuynh0245@gmail.com>
2025-02-17 14:00:30 +0700

    feat: send json from LoRa to server

--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -1,93 +1,85 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
 #include <SPI.h>
 #include <LoRa.h>
 
 const char *ssid = "Wifi_SSID";
 const char *password = "Password";
-const String id = "3fa85f64-5717-4562-b3fc-2c963f66afa6";
-const char *server_url = ("https://trashcan-api.arisavinh.dev/device/" + id + "/data").c_str();
+const char *server_url = "https://trashcan-api.arisavinh.dev/device/3fa85f64-5717-4562-b3fc-2c963f66afa6/data";
 
 #define LORA_SS_PIN 15
 #define LORA_RESET_PIN 16
 #define LORA_DIO0_PIN 4
 
-void send_data_to_server(String json_string)
-{
-  HTTPClient http;
-  WiFiClient client;
+volatile bool lora_data_received = false;
+String lora_received_data = "";
 
+void send_data_to_server() {
+  BearSSL::WiFiClientSecure client;
+  client.setInsecure();
+
+  HTTPClient http;
   http.begin(client, server_url);
   http.addHeader("Content-Type", "application/json");
 
-  int httpResponseCode = http.POST(json_string);
+  // Serial.println("Sending POST request with JSON: " + json_string);
 
-  if (httpResponseCode > 0)
-  {
+  int httpResponseCode = http.POST(lora_received_data);
+  Serial.println("HTTP Response code: " + String(httpResponseCode));
+
+  if (httpResponseCode > 0) {
     String response = http.getString();
-    Serial.println("HTTP Response code: " + String(httpResponseCode));
     Serial.println("Response: " + response);
-  }
-  else
-  {
+  } else {
     Serial.println("Error sending data: " + String(httpResponseCode));
   }
 
   http.end();
 }
 
-void setup()
-{
-  Serial.begin(115200);
+void on_receive(int packet_size) {
+  if (packet_size == 0) return;
+
+  lora_received_data = "";
+  while (LoRa.available()) {
+    lora_received_data += (char)LoRa.read();
+  }
+  lora_data_received = true;
+}
+
+void setup() {
+  Serial.begin(9600);
 
   LoRa.setPins(LORA_SS_PIN, LORA_RESET_PIN, LORA_DIO0_PIN);
-
-  if (!LoRa.begin(915E6))
-  {
+  if (!LoRa.begin(433E6)) {
     Serial.println("LoRa initialization failed!");
   }
-  Serial.println("LoRa initialization successful!");
+  LoRa.onReceive(on_receive);
+  LoRa.receive();
 
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("\nConnected to WiFi");
 }
 
-const char* read_lora() {
-  int packetSize = LoRa.parsePacket();
-
-  if(packetSize == 0)
-    return NULL;
-
-
-  String received_data = "";
-  while (LoRa.available())
-  {
-    received_data += (char)LoRa.read();
+void loop() {
+  if (lora_data_received) {
+    lora_data_received = false;
+    if (WiFi.status() == WL_CONNECTED) {
+      send_data_to_server();
+    }
   }
-  Serial.println("Received LoRa data: " + received_data);
 
-  return received_data.c_str();
-}
-
-void loop()
-{
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    const char *received_data = read_lora();
-    if(received_data != NULL)
-      send_data_to_server(received_data);
-  }
-  else
-  {
+  if (WiFi.status() != WL_CONNECTED) {
     Serial.println("WiFi connection lost");
     WiFi.begin(ssid, password);
   }
 
-  delay(100);
+  delay(1000);
 }
