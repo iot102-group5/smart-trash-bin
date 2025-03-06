d7351c3f2ceead0c5a221cc915de531ec8d853c6
akagiyuu <khanghuynh0245@gmail.com>
2025-03-06 10:10:59 +0700

    feat: only send new state when it's different from prev

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -4,99 +4,111 @@
 #include <Servo.h>
 #include <ArduinoJson.h>
 
-#define OBJECT_TRIG_PIN     8
-#define OBJECT_ECHO_PIN     7
-#define TRASH_TRIG_PIN      6
-#define TRASH_ECHO_PIN      5
-#define SERVO_PIN           3
+#define OBJECT_TRIG_PIN 8
+#define OBJECT_ECHO_PIN 7
+#define TRASH_TRIG_PIN 6
+#define TRASH_ECHO_PIN 5
+#define SERVO_PIN 3
 
-#define BAUD_RATE           9600
-#define LORA_FREQUENCY      433E6
-#define SOUND_SPEED         0.0334  // cm/s
-#define DATA_SEND_DELAY     5000    // ms
-#define TRASH_CAN_HEIGHT    40      // cm
-#define OPEN_DISTANCE       10      // cm
+#define BAUD_RATE 9600
+#define LORA_FREQUENCY 433E6
+#define SOUND_SPEED 0.0334 // cm/s
+#define DATA_SEND_DELAY 5000 // ms
+#define TRASH_CAN_HEIGHT 40 // cm
+#define OPEN_DISTANCE 10 // cm
+
+struct State {
+	float trash_level;
+	bool is_open;
+};
+bool operator==(const struct State &a, const struct State &b)
+{
+	return a.is_open == b.is_open && a.trash_level == b.trash_level;
+}
+
+struct State prev_state = {
+	.trash_level = 0,
+	.is_open = false,
+};
 
 Servo servo;
 
 void onTxDone()
 {
-    Serial.println("Send data success");
+	Serial.println("Send data success");
 }
 
 void setup()
 {
-    Serial.begin(BAUD_RATE);
+	Serial.begin(BAUD_RATE);
 
-    pinMode(OBJECT_TRIG_PIN, OUTPUT);
-    pinMode(OBJECT_ECHO_PIN, INPUT);
+	pinMode(OBJECT_TRIG_PIN, OUTPUT);
+	pinMode(OBJECT_ECHO_PIN, INPUT);
 
-    pinMode(TRASH_TRIG_PIN, OUTPUT);
-    pinMode(TRASH_ECHO_PIN, INPUT);
+	pinMode(TRASH_TRIG_PIN, OUTPUT);
+	pinMode(TRASH_ECHO_PIN, INPUT);
 
-    servo.attach(SERVO_PIN, 600, 2300);
+	servo.attach(SERVO_PIN, 600, 2300);
 
-    if (!LoRa.begin(LORA_FREQUENCY))
-    {
-        Serial.println("LoRa init failed!");
-    }
-    LoRa.onTxDone(onTxDone);
+	if (!LoRa.begin(LORA_FREQUENCY)) {
+		Serial.println("LoRa init failed!");
+	}
+	LoRa.onTxDone(onTxDone);
 }
 
 float get_distance(int trig_pin, int echo_pin)
 {
-    digitalWrite(trig_pin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trig_pin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trig_pin, LOW);
+	digitalWrite(trig_pin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(trig_pin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trig_pin, LOW);
 
-    float duration = pulseIn(echo_pin, HIGH);
-    float distance = duration / 2 * SOUND_SPEED;
+	float duration = pulseIn(echo_pin, HIGH);
+	float distance = duration / 2 * SOUND_SPEED;
 
-    return distance;
+	return distance;
 }
 
-float get_trash_level()
+struct State get_state()
 {
-    float distance = get_distance(TRASH_TRIG_PIN, TRASH_ECHO_PIN);
-    float trash_level = (1 - distance / TRASH_CAN_HEIGHT) * 100;
-    trash_level = constrain(trash_level, 0, 100);
-    return trash_level;
+	float distance = get_distance(TRASH_TRIG_PIN, TRASH_ECHO_PIN);
+	float trash_level = (1 - distance / TRASH_CAN_HEIGHT) * 100;
+	trash_level = constrain(trash_level, 0, 100);
+
+	bool is_open = get_distance(OBJECT_TRIG_PIN, OBJECT_ECHO_PIN) <= OPEN_DISTANCE;
+
+	return {
+		.trash_level = trash_level,
+		.is_open = is_open,
+	};
 }
 
-bool get_is_open()
+void sync_state(struct State &data)
 {
-    return get_distance(OBJECT_TRIG_PIN, OBJECT_ECHO_PIN) <= OPEN_DISTANCE;
+	servo.write(data.is_open ? 180 : 0);
 }
 
-void set_trash_can(bool is_open)
+void lora_transmit(struct State &state)
 {
-    servo.write(is_open ? 90 : 0);
-}
+	JsonDocument doc;
 
-void lora_transmit(float trash_level, bool is_open)
-{
-    JsonDocument doc;
+	doc["trash_level"] = state.trash_level;
+	doc["is_open"] = state.is_open;
 
-    doc["is_open"] = is_open;
-    doc["trash_level"] = trash_level;
-
-    LoRa.beginPacket();
-    serializeJson(doc, LoRa);
-    LoRa.endPacket(true);
+	LoRa.beginPacket();
+	serializeJson(doc, LoRa);
+	LoRa.endPacket(true);
 }
 
 void loop()
 {
-    float trash_level = get_trash_level();
-    Serial.println(trash_level);
+	struct State state = get_state();
+	if (state == prev_state) {
+		return;
+	}
 
-    bool is_open = get_is_open();
-    set_trash_can(is_open);
-    Serial.println(is_open);
-
-    lora_transmit(trash_level, is_open);
-
-    delay(DATA_SEND_DELAY);
+    prev_state = state;
+	lora_transmit(state);
+    sync_state(state);
 }
